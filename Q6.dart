58fe0333982c71fd2715c0da75931306f06aabfe
usmanghani2023 <usman.efulife@gmail.com>
2023-07-02 08:03:26 -0700

    Ans6

--- Q6.dart
+++ Q6.dart
@@ -1,14 +1,19 @@
 void main() {
-  var alphabet = 'u';
-
-  if (alphabet == 'a' ||
-      alphabet == 'e' ||
-      alphabet == 'i' ||
-      alphabet == 'o' ||
-      alphabet == 'u') {
-    print('The alphabet $alphabet is a vowel');
+  int customer_id = 1001;
+  String customer_name = 'james';
+  int units_consumed = 399;
+  double bill_amount = 0.0;
+  if (units_consumed <= 199) {
+    bill_amount = units_consumed * 1.20;
+  } else if (units_consumed >= 200 && units_consumed < 400) {
+    bill_amount = units_consumed * 1.50;
+  } else if (units_consumed >= 400 && units_consumed < 500) {
+    bill_amount = units_consumed * 1.80;
   } else {
-    // The alphabet is a consonant
-    print('The alphabet $alphabet is a consonant');
+    bill_amount = units_consumed * 2.0;
   }
+  print('customer Id =$customer_id');
+  print('customer name = $customer_name');
+  print('Unit consumed = $units_consumed');
+  print('Total bill =$bill_amount');
 }
