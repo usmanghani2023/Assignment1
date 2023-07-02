f20e800f8256d6248cbe7789a9e80a3943dee6e5
usmanghani2023 <usman.efulife@gmail.com>
2023-07-02 07:04:14 -0700

    Ans1 of assignment1

---
+++ Q1.dart
@@ -0,0 +1,10 @@
+void main() {
+  int length = 5;
+  int breadth = 6;
+
+  if (length == breadth) {
+    print('The values represent a square.');
+  } else {
+    print('The values represent a rectangle.');
+  }
+}
