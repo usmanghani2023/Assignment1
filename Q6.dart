aa10be9f6cf244ac8fb11eee38bd0d15bbce6ec0
usmanghani2023 <136729528+usmanghani2023@users.noreply.github.com>
2023-07-02 11:10:52 -0700

    Create Q6.dart

---
+++ Q6.dart
@@ -0,0 +1,12 @@
+void main() {
+  var alphabet = 'a';
+  if (alphabet == 'a' ||
+      alphabet == 'e' ||
+      alphabet == 'i' ||
+      alphabet == 'o' ||
+      alphabet == 'u') {
+    print('The alphabet $alphabet is a vowel');
+  } else {
+    print('The alphabet $alphabet is a consonant');
+  }
+}
