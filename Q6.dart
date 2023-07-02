6cf95de92d28a67892907c392d2b7c9f47db0be7
usmanghani2023 <usman.efulife@gmail.com>
2023-07-02 07:35:13 -0700

    Ans6

---
+++ Q6.dart
@@ -0,0 +1,14 @@
+void main() {
+  var alphabet = 'u';
+
+  if (alphabet == 'a' ||
+      alphabet == 'e' ||
+      alphabet == 'i' ||
+      alphabet == 'o' ||
+      alphabet == 'u') {
+    print('The alphabet $alphabet is a vowel');
+  } else {
+    // The alphabet is a consonant
+    print('The alphabet $alphabet is a consonant');
+  }
+}
